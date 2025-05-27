10941939787fea7a853cb9feab713453fee5dd1a
NotsoanoNimus <github@xmit.xyz>
2025-05-27 06:33:23 -0400

    rust: project init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+
+
+
+fn main()
+{
+    /* === Generic initialization. === */
+    /* === Read and parse vehicle DBC file. === */
+    /* === Read and parse dashboard configuration with profiles. === */
+    /* === Validate profile mappings with parsed DBC details and references. === */
+    /* === Load required widgets for profile. === */
+    /* === Start the CAN socket/listener. === */
+    /* === Start the main rendering loop which changes based on CAN data and mappings. === */
+    println!("Hello, world!");
+}
